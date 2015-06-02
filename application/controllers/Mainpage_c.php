ff36463997869176cce9558c4392f5c41620245c
Park Gee young <rezarafiqmz@gmail.com>
2015-06-02 20:37:27 +0700

    delete set header

--- application/controllers/Mainpage_c.php
+++ application/controllers/Mainpage_c.php
@@ -6,12 +6,6 @@ class Mainpage_c extends CI_Controller {
         parent:: __construct();
         date_default_timezone_set("Asia/Jakarta");
 
-        $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
-      	('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-    	$this->output->set_header('Pragma: no-cache');
-    	$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
-    	$this->output->set_header();
-
 		$this->load->model('main_m');
 		$this->load->library('timeago');
 		$this->pbj();
